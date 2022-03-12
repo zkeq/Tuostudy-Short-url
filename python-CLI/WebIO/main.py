92978a5de079831fc12bbabf452a8f9d5f07adf6
Zkeq <learnonly@learnonly.xyz>
2022-03-12 21:56:41 +0800

    PyWebIO 测试分支（代码稀烂）

---
+++ python-CLI/WebIO/main.py
@@ -0,0 +1,146 @@
+import hashlib
+import time
+import urllib.parse
+from functools import partial
+
+import pywebio
+import requests
+from pywebio import start_server, config
+from pywebio.input import *
+from pywebio.output import *
+
+
+def login():
+    """login page"""
+    pywebio.output.clear()
+    put_markdown("""# 登录 短链管理系统 🚀""")
+    info_login = input_group('', [
+        input("请输入你的用户名", name="users"),
+        input("请输入你的密码", name="password", type=PASSWORD),
+    ])
+    if info_login["users"] == 'admin' and info_login["password"] == 'admin':
+        return True
+    else:
+        return False
+
+
+def get_now_dict():
+    dict_data = requests.get('http://127.0.0.1:3211/').json()
+    return dict_data
+
+
+def del_el(el):
+    result = requests.delete('http://127.0.0.1:3211/delete/{0}'.format(el)).json()
+    if result['result']:
+        pywebio.output.toast("删除成功", duration=3, color="success")
+        close_popup()
+        get_table()
+        while True:
+            login_su()
+    else:
+        pywebio.output.toast("删除失败", duration=3, color="warn")
+
+
+def edit_row(choice, row):
+    dict_data = get_now_dict()
+    list_data = get_now_list(dict_data)
+    row = list_data[row - 1][1]
+    url = dict_data[row]
+    list_data = [(row, url)]
+    get_table()
+    if choice == '🖊️':
+        popup("编辑短链", [
+            put_markdown("""## 您当前选择的短链是: {0}""".format(row)),
+            put_table(list_data, ["名称", "内容"]),
+            put_markdown("""#### 将本窗口关闭后请在下方的输入框中更新！""".format(row, url)),
+            put_buttons(['我知道了，关闭弹窗在并下方更新。'], onclick=lambda _: close_popup()),
+        ])
+        url_info = input_group('编辑短链:', [input("请输入对应的长链: ", name="url", type=URL)])
+        dict_data = {"name": row, "url": url_info["url"]}
+        list_data = [(row, url_info["url"])]
+        data = requests.post('http://127.0.0.1:3211/update/', json=dict_data).json()
+        print(data)
+        if data['result']:
+            popup("更新成功",
+                  [
+                      put_markdown("""## 更新后的短链信息为:"""),
+                      put_table(list_data, ["名称", "内容"]),
+                      put_buttons(['关闭弹窗'], onclick=lambda _: close_popup())
+                  ])
+        else:
+            popup("更新失败", [
+                put_buttons(['关闭弹窗'], onclick=lambda _: close_popup())])
+        while True:
+            login_su()
+    if choice == '❌':
+        pywebio.output.toast("您确认要删除吗？", duration=3, color="warn")
+        popup("编辑短链", [
+            put_markdown("""## 警告: 当前的操作属于敏感操作！""", color="red"),
+            put_markdown("""### 当前选择的短链是: {0}""".format(row)),
+            put_table(list_data, ["名称", "内容"]),
+            put_markdown("""您确认要删除吗？"""),
+            put_buttons(['关闭弹窗'], onclick=lambda _: [close_popup()]),
+            put_buttons(['确认删除'], onclick=lambda _: [del_el(row)])
+        ])
+        while True:
+            login_su()
+
+
+def get_now_list(dict_data):
+    list_data = []
+    num = 1
+    for i in dict_data:
+        dict_data[i] = urllib.parse.unquote(dict_data[i])
+        list_data.append([num, i, dict_data[i],
+                          put_buttons(['🖊️', '❌'], group=True, small=True, onclick=partial(edit_row, row=num))])
+        num += 1
+    return list_data
+
+
+def get_table():
+    data_dict = get_now_dict()
+    data_list = get_now_list(data_dict)
+    pywebio.output.clear()
+    put_markdown("""# 欢迎使用 短链管理系统 🚀 """)
+    put_table(data_list, ["No", "名称", "内容", "操作"])
+    return data_dict, data_list
+
+
+def login_su():
+    get_table()
+    info = select('添加新短链:', ['自定义生成短链', '随机生成的短链'])
+    if info == '自定义生成短链':
+        short_info = input_group('自定义生成短链:', [
+            input("请输入短链: ", name="name"),
+            input("请输入对应的长链: ", name="url", type=URL),
+        ])
+    else:
+        short_info = input_group('随机生成的短链:', [
+            input("要生成短链的位数: ", name="name", type=NUMBER, min=1, max=32),
+            input("请输入对应的长链: ", name="url", type=URL),
+        ])
+        short_info["name"] = get_time_hash()[:short_info["name"]]
+    put_markdown("""# 生成结果""")
+    print(short_info)
+    data = requests.post('http://127.0.0.1:3211/new/', json=short_info)
+    print(data.text)
+
+
+@config(theme="yeti")
+def main():
+    lg = login()
+    if lg:
+        while True:
+            login_su()
+    else:
+        put_markdown("""# 登录失败""")
+
+
+def get_time_hash():
+    time_str = str(time.time())
+    time_hash = hashlib.md5(time_str.encode('utf-8')).hexdigest()
+    return time_hash
+
+
+if __name__ == '__main__':
+    start_server(main, debug=True, port=3985, cdn="https://s-bj-2220-tuo-admin.oss.dogecdn.com/")
